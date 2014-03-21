e61dcd23733350654e8a2d340ea9339e0a9d1d59
raul.ramos <raul.ramos@atrapalo.com>
2014-03-21 17:46:30 +0100

    including guards

---
+++ src/ChocolateFeastException.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: raul.ramos
+ * Date: 21/03/14
+ * Time: 17:41
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ChocolateFeastException extends Exception {}
